f3134d1f825b6d46ff24f50534c383cd02c35b43
Francis <francis.alvarezfigueroa@gmail.com>
2025-06-07 16:49:00 -0400

    Creacion clases y subclases

--- src/main/java/cl/duoc/bankeurope/modelos/Cuenta.java
+++
@@ -1,5 +0,0 @@
-package cl.duoc.bankeurope.modelos;
-
-public class Cuenta {
-    // en esta clase se agregan los atributos y metodos de cuenta.
-}
---
+++ src/main/java/cl/duoc/bankeurope/modelos/CuentaBancaria.java
@@ -0,0 +1,42 @@
+package cl.duoc.bankeurope.modelos;
+
+public  abstract class CuentaBancaria {
+
+    protected long saldo;
+    protected long numeroCuenta;
+
+
+
+    // CONSTRUCTOR Y SOBRECARGA DE CONSTRUCTOR _______________________________________________________
+    public CuentaBancaria(long saldo, long numeroCuenta) {
+        this.saldo = saldo;
+        this.numeroCuenta = numeroCuenta;
+    }
+
+    public CuentaBancaria (long numeroCuenta){
+        this.numeroCuenta=numeroCuenta;
+        saldo=0L;
+    }
+
+    // GETTERS Y SETTERS _____________________________________________________________________________
+
+    public long getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(long saldo) {
+        this.saldo = saldo;
+    }
+
+    public long getNumeroCuenta() {
+        return numeroCuenta;
+    }
+
+    public void setNumeroCuenta (long numeroCuenta) {
+        this.numeroCuenta = numeroCuenta;
+    }
+
+    // MÉTODO ABSTRACTO PARA CALCULAR INTERÉS_________________________________________________________
+
+    public abstract double calcularInteres ();  // solo declaracion, obliga implementacion en subclases
+}
