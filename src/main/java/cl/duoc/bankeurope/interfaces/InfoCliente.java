329020958ff49d6c3caf883e03209429c0a5034c
Francis <francis.alvarezfigueroa@gmail.com>
2025-06-08 19:09:31 -0400

    Cambios a subclases

--- src/main/java/cl/duoc/bankeurope/interfaces/InfoCliente.java
+++ src/main/java/cl/duoc/bankeurope/interfaces/InfoCliente.java
@@ -2,5 +2,6 @@ package cl.duoc.bankeurope.interfaces;
 
 public interface InfoCliente {
 
+    // todo: separa funcionalidades ver info cliente y ver info cuenta (2 interfaces diferentes)
     public void mostrarInformacionCliente();
 }
